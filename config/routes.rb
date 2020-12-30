5c6d33fe63dd2704caedde4db010072f74255535
mippy98 <70516972+mippy98@users.noreply.github.com>
2020-12-30 22:48:18 +0900

    フロント実装準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  root to: "posts#index"
+  get 'posts/index'
+  devise_for :users
+>>>>>>> Stashed changes
 end
