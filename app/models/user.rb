7ce81091e531ed86d8ea6cd96b3f9c848a3f21c6
mippy98 <70516972+mippy98@users.noreply.github.com>
2020-12-30 22:02:15 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+       
+  with_options presence: true do
+    validates :name
+    validates :email, uniqueness: {case_sensitive: false}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, confirmation: true, length: { minimum: 6 } 
+  end
 end
