e9e0bb496dbf071a91855d76ff3cf59d5ee1890c
mippy98 <70516972+mippy98@users.noreply.github.com>
2021-01-30 21:55:23 +0900

    マイページにページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @name = current_user.name
-    @posts = current_user.posts
+    @posts = current_user.posts.page(params[:page]).per(10)
   end
 end
