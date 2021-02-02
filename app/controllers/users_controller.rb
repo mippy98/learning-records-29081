71b8bab2411c243c6a35b18177e783978ccbcee3
mippy98 <70516972+mippy98@users.noreply.github.com>
2021-02-02 18:45:10 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @name = current_user.name
-    @posts = current_user.posts.page(params[:page]).per(10)
+    @posts = current_user.posts.order("created_at DESC").page(params[:page]).per(10)
   end
 end
